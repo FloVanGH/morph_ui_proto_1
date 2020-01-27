f7b54979f20c95a2a4d9baa47188a20a58fdd9b4
Florian Blasius <florian.blasius@ergosign.de>
2020-01-27 08:11:02 +0100

    Add wasm32 target.

--- src/platform/web/mod.rs
+++ src/platform/web/mod.rs
@@ -1,3 +1,13 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 /// Platform dependent main loop.
 pub fn main_loop<R: FnMut()>(mut run: R) {
   
