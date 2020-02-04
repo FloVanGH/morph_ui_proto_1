6d0e18ca4131199b90b4b84ce03cea40d465fbd3
Florian Blasius <flovanpt@posteo.de>
2020-02-04 06:11:24 +0100

    Edit vs code config.

--- examples/minimal_cortex_m.rs
+++ examples/minimal_cortex_m.rs
@@ -47,7 +47,7 @@ fn main() -> ! {
 
     // exit QEMU
     // NOTE do not run this on hardware; it can corrupt OpenOCD state
-    debug::exit(debug::EXIT_SUCCESS);
+    // debug::exit(debug::EXIT_SUCCESS);
 
     loop {}
 }
