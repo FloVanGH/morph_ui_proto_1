58cf5d565f3c52dea6bdc81f6f2cd64736b7176a
Florian Blasius <flovanpt@posteo.de>
2020-02-05 21:32:41 +0100

    Try to integrate st7735-rs crate.

--- examples/minimal_cortex_m.rs
+++ examples/minimal_cortex_m.rs
@@ -1,53 +1,66 @@
+#![feature(alloc)]
+#![feature(alloc_error_handler)]
+
 #![no_std]
 #![no_main]
 
 #[path = "minimal.rs"]
 mod minimal;
 
-extern crate panic_halt;
+use panic_halt as _;
 
-use embedded_graphics::fonts::{Font6x8, Text};
-use embedded_graphics::pixelcolor::Rgb565;
-use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::{Circle, Line};
-use embedded_graphics::style::{PrimitiveStyle, TextStyle};
+use core::alloc::Layout;
 
-// Only used for examples - this would be replaced by the driver for your chosen display
-use embedded_graphics::mock_display::MockDisplay as Display;
-use cortex_m_rt::entry;
+use alloc_cortex_m::CortexMHeap;
+
+// this is the allocator the application will use
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+
+use cortex_m::asm;
 use cortex_m_semihosting::debug;
+use cortex_m_rt::entry;
+
+use st7735::color::{Color, DefaultColor};
+use st7735::ST7734;
+
+use stm32f1xx_hal::{
+    prelude::*,
+    pac,
+    delay::Delay,
+}; 
+
+use embedded_hal::digital::v2::OutputPin;
 
 #[entry]
 fn main() -> ! {
-     // Create a display object to draw into
-    // This will be whichever display driver you decide to use, like the SSD1306, SSD1351, etc
-    let mut display = Display::new();
+    let dp = pac::Peripherals::take().unwrap();
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
 
-    // Draw a circle centered at (64, 64) with a radius of 64 and a white 1px stroke
-    Circle::new(Point::new(64, 64), 64)
-        .into_styled(PrimitiveStyle::with_stroke(Rgb565::WHITE, 1))
-        .draw(&mut display);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let cp = cortex_m::Peripherals::take().unwrap(); 
+    let mut delay = Delay::new(cp.SYST, clocks);
 
-    // Draw a white 1px thick line from (64, 64) to (0, 64)
-    Line::new(Point::new(64, 64), Point::new(0, 64))
-        .into_styled(PrimitiveStyle::with_stroke(Rgb565::WHITE, 1))
-        .draw(&mut display);
 
-    // Draw a red 1px line from (64, 64) to (80, 80)
-    Line::new(Point::new(64, 64), Point::new(80, 80))
-        .into_styled(PrimitiveStyle::with_stroke(Rgb565::RED, 1))
-        .draw(&mut display);
+    // Acquire the GPIOA peripheral
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
 
-    // Print "Hello world!" in a white 6x8 pixel font with the top left corner positioned at (5, 50)
-    Text::new("Hello World!", Point::new(5, 50))
-        .into_styled(TextStyle::new(Font6x8, Rgb565::WHITE))
-        .draw(&mut display);
+    // let clk = gpiob.adc5;
         
     minimal::start_example();
 
     // exit QEMU
     // NOTE do not run this on hardware; it can corrupt OpenOCD state
     // debug::exit(debug::EXIT_SUCCESS);
+
+    loop {}
+}
+
+// define what happens in an Out Of Memory (OOM) condition
+#[alloc_error_handler]
+fn alloc_error(_layout: Layout) -> ! {
+    asm::bkpt();
 
     loop {}
 }
